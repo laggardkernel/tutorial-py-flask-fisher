0cd0a01847503e76239aee10d6778c88c785f95e
laggardkernel <laggardkernel@gmail.com>
2018-10-13 09:26:19 +0800

    New: custom response headers with make_response

--- fisher.py
+++ fisher.py
@@ -1,15 +1,19 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-from flask import Flask
+from flask import Flask, make_response
 
 app = Flask(__name__)
+app.config.from_object('config')
 
 
-# @app.route('/hello')
+@app.route('/hello')
 def hello():
-    return 'Hello, world'
+    headers = {'Content-Type': 'text/plain'}
+    r = make_response('<html></html>', 404)
+    r.headers = headers
+    return r
 
 
-app.add_url_rule('/hello', view_func=hello)
+# app.add_url_rule('/hello', view_func=hello)
 
-app.run(host='0.0.0.0', port=81)
+app.run(host='0.0.0.0', debug=app.config['DEBUG'], port=81)
