2027ef011bb768e3f9ac9735b9a2510b0617647e
laggardkernel <laggardkernel@gmail.com>
2019-01-09 20:19:44 +0800

    Fix login redirection
    Avoid jump back to login page.

--- app/auth/routes.py
+++ app/auth/routes.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 from . import auth
+from werkzeug.urls import url_parse
 from flask import render_template, request, redirect, url_for, flash
 from flask_login import login_user, logout_user, login_required
 from .forms import RegistrationForm, LoginForm
-from .utils import get_redirect_target
 from app.models import User
 from app import db
 
@@ -31,12 +31,16 @@ def login():
     form = LoginForm(request.form)
     if request.method == "POST" and form.validate():
         user = User.query.filter_by(email=form.email.data).first()
-        # TODO: remove debugging print
-        # print(user)
         if user is not None and user.verify_password(form.password.data):
             login_user(user, remember=form.remember_me.data)
             # force redirect to the home page if the "next" url is not safe
-            next = get_redirect_target() or url_for("web.index")
+            next = request.args.get("next")
+            if (
+                not next
+                or url_parse(next).netloc != ""
+                or url_parse(next).path != "/login"
+            ):
+                next = url_for("web.index")
             return redirect(next)
         else:
             flash("账户或密码错误")
