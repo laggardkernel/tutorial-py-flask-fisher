ec0582ea2369eff72812b3f3d61a4a13f9836910
laggardkernel <laggardkernel@gmail.com>
2019-01-12 16:23:09 +0800

    Fix gift list page and wish list page

--- app/static/js/book-detail.js
+++ app/static/js/book-detail.js
@@ -2,5 +2,5 @@
 
 $(document).on('confirmation', '.remodal', function () {
     var isbn = $('#isbn').text()
-    window.location.href='/gifts/book/' + isbn
+    window.location.href='/gift/' + isbn
 });
